de01c8269c38326973a1871b3b9a9356522f3c1c
sharon <sharzheng@gmail.com>
2023-07-29 16:34:34 -0700

    fix overflow

--- script.js
+++ script.js
@@ -7,9 +7,13 @@ const promptWrapper = document.getElementById('promptWrapper');
 
 let PLAYBACK_CONST = 2000;
 let showPrompt = true
+let pageReady = false
+let maxHeight;
 
 window.onscroll = () => {
-    // videoEl.pause();
+    videoEl.pause();
+	
+	// fade prompt away when scrolling for a bit
 	if (pageReady && showPrompt && window.scrollY > 500) {
 		promptWrapper.style.opacity = (1 - (window.scrollY/900)).toFixed(1)
 		if (window.scrollY > 1500) {
@@ -17,10 +21,20 @@ window.onscroll = () => {
 			promptWrapper.style.opacity = 0
 		}
 	}
-};
 
-let pageReady = false
+	// figure out actual height needed
+	if (maxHeight == null && window.scrollY > videoEl.duration * PLAYBACK_CONST) {
+		maxHeight = window.scrollY + 500
+	}
+
+	// restrict scrolling past
+	if (window.scrollY > maxHeight) {
+		bodyEl.style.maxHeight = `${maxHeight * 1.5}px`
+		window.scrollTo(0, maxHeight)
+	}
+};
 		
+// message prompt animation logic
 prompt.addEventListener('animationiteration', function() {
 	if (bodyEl.classList.contains('video-loaded')) {
 		prompt.classList.remove('loading')
@@ -41,8 +55,8 @@ prompt.addEventListener('animationiteration', function() {
 // trigger once
 videoEl.addEventListener("canplaythrough", () => {
 	bodyEl.classList.add('video-loaded')
-	bodyEl.style.height = `${videoEl.duration * PLAYBACK_CONST * 1.4}px`;
-	bodyHeight = videoEl.duration * PLAYBACK_CONST * 1.4
+	bodyEl.style.height = `${videoEl.duration * PLAYBACK_CONST * 1.5}px`;
+	bodyHeight = videoEl.duration * PLAYBACK_CONST * 1.5
 }, {once: true});
 
 // back to top on refresh
