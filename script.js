d94fd9546980dcddc59c6b2d697d1f349ccaaa79
sharon <sharzheng@gmail.com>
2023-07-29 15:56:35 -0700

    prompt message with animation

--- script.js
+++ script.js
@@ -2,22 +2,48 @@ const videoEl = document.querySelector('video');
 const videoWrapperEl = document.querySelector('main');
 const bodyEl = document.querySelector('body');
 
-const PLAYBACK_CONST = 500;
+const prompt = document.getElementById('prompt');
+const promptWrapper = document.getElementById('promptWrapper');
+
+let PLAYBACK_CONST = 2000;
+let showPrompt = true
 
 window.onscroll = () => {
-    videoEl.pause();
+    // videoEl.pause();
+	if (pageReady && showPrompt && window.scrollY > 500) {
+		promptWrapper.style.opacity = (1 - (window.scrollY/900)).toFixed(1)
+		if (window.scrollY > 1500) {
+			showPrompt = false
+			promptWrapper.style.opacity = 0
+		}
+	}
 };
 
-setInterval(() => {
-    videoEl.currentTime = window.scrollY/PLAYBACK_CONST;
-}, 40);
+let pageReady = false
+		
+prompt.addEventListener('animationiteration', function() {
+	if (bodyEl.classList.contains('video-loaded')) {
+		prompt.classList.remove('loading')
+		prompt.classList.add('fade-out')
 
+		setTimeout(() => {
+			prompt.classList.remove('fade-out')
+			prompt.textContent = 'Scroll to read'
+			setTimeout(() => {
+				pageReady = true
+				bodyEl.style.overflowY = 'scroll'
+			}, 1000)
+			prompt.classList.add('fade-in')
+		}, 500)
+	}
+})
+
+// trigger once
 videoEl.addEventListener("canplaythrough", () => {
-	setTimeout(() => (
-		bodyEl.classList.add('video-loaded')
-	), 3000); // match with progress bar animation delay value
+	bodyEl.classList.add('video-loaded')
 	bodyEl.style.height = `${videoEl.duration * PLAYBACK_CONST * 1.4}px`;
-});
+	bodyHeight = videoEl.duration * PLAYBACK_CONST * 1.4
+}, {once: true});
 
 // back to top on refresh
 window.onbeforeunload = () => {
@@ -26,3 +52,8 @@ window.onbeforeunload = () => {
 
 // pause immediately onload
 videoEl.pause();
+
+// scrolling => video playing logic
+setInterval(() => {
+	videoEl.currentTime = window.scrollY/PLAYBACK_CONST;
+}, 40);
